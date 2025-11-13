4bff2b7c68c217adf7c6d3fd374c52ebb605eba7
not rkorom <121223458+H4RmYxDD@users.noreply.github.com>
2025-11-13 09:22:10 +0100

    redukalni kell az idot

---
+++ konzolapplikacio/Bicikli.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konzolapplikacio
+{
+    public class Bicikli
+    {
+    public List<Bicikli> BicikliList;
+        public string nev;
+    public DateTime kezdes;
+    public DateTime vegzes;
+    public string Type;
+    public TimeSpan kulonbseg;
+       public double osszeg;
+    public Bicikli(DateTime kezdes1, DateTime vegzes1, string tipus) { 
+        kezdes = kezdes1;
+        vegzes = vegzes1;
+        Type = tipus;
+        TimeSpan mennyit = vegzes - kezdes;
+    }
+    }
+}
--- konzolapplikacio/Program.cs
+++ konzolapplikacio/Program.cs
@@ -1,41 +1,42 @@
-﻿List<string> data = new List<string>();
+﻿using konzolapplikacio;
+
+List<string> data = new List<string>();
 
 var lines = File.ReadAllLines("vizibicikli.csv").Skip(1);
 List<int> futas = new List<int>();
-TimeSpan mennyitMent = TimeSpan.MinValue;
+TimeSpan mennyitMent = TimeSpan.Zero;
+    var biciklik = new List<Bicikli>();
+double osszesKereset = 0;
 
 
 foreach (var l in lines)
 {
-    data = l.Split(',').ToList();
-    var type = l[1].ToString();
+    data = l.Split(", ").ToList();
+    var type = data[1].ToString();
     var nap = 12;
-    if (int.Parse(data[2]) > int.Parse(data[4]))
+    if (int.Parse(data[2]) >= int.Parse(data[4]))
     {
         nap = 13;
     }
-    Console.WriteLine(data[2]);
     DateTime kezdes = new DateTime(2025, 11, 12, int.Parse(data[2]), int.Parse(data[3]), 0);
     DateTime vegzes = new DateTime(2025, 11, nap, int.Parse(data[4]), int.Parse(data[5]), 0);
-    mennyitMent += (vegzes - kezdes);
-    Bicikli bicikli = new Bicikli(kezdes, vegzes,type);
-    Console.WriteLine(kezdes.ToString());
-    Console.WriteLine(vegzes.ToString());
-    Console.WriteLine(mennyitMent);
-    //bicikli.BicikliList.Add(bicikli);
-}
+    var bicikli = new Bicikli(kezdes, vegzes, type);
 
-public class Bicikli
-{
-    public List<Bicikli> BicikliList;
-    DateTime kezdes;
-    DateTime vegzes;
-    string Type;
-    TimeSpan kulonbseg;
-    public Bicikli(DateTime kezdes1, DateTime vegzes1, string tipus) { 
-        kezdes = kezdes1;
-        vegzes = vegzes1;
-        Type = tipus;
-        TimeSpan mennyit = vegzes - kezdes;
-    }
+    var kulonbseg = (vegzes - kezdes);
+    mennyitMent += kulonbseg;
+    bicikli.nev = data[0];
+    bicikli.kezdes = kezdes;
+    bicikli.vegzes = vegzes;
+    bicikli.Type = type;
+    var fizetendo = Math.Ceiling(kulonbseg.TotalMinutes / 30) * 1000;
+    bicikli.osszeg = fizetendo;
+    biciklik.Add(bicikli);
 }
+ 
+var groupedBiciklik = biciklik.GroupBy(x=> x.Type).Select(g=>new {id=g.Key, fizetendo = g.Sum(b=>b.osszeg)});
+var legtobb = groupedBiciklik.MaxBy(x=>x.fizetendo);
+Console.WriteLine("a legtobbet keresett bicikli:" + legtobb);
+Console.WriteLine("melyik bicikli adatait szeretne lekerni?");
+var keresendo = Console.ReadLine().ToUpper();
+var talalat = groupedBiciklik.FirstOrDefault(a=>a.id.Equals(keresendo));
+Console.WriteLine(talalat);
